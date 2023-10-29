8b43e5a6b01b48e09337bde4a4908ecea15ea630
Osama <147293504+Osmaaa@users.noreply.github.com>
2023-10-29 14:10:36 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,78 @@
+import os
+import sys
+import json
+import time
+import requests
+import websocket
+from keep_alive import keep_alive
+
+status = "dnd" 
+custom_status = "Simsalabim..." 
+
+usertoken = os.getenv("TOKEN")
+if not usertoken:
+    print("Adicione sua token na ENV")
+    sys.exit()
+
+headers = {"Authorization": usertoken, "Content-Type": "application/json"}
+
+validate = requests.get("https://canary.discordapp.com/api/v9/users/@me", headers=headers)
+if validate.status_code != 200:
+    print("Sua token esta invalida")
+    sys.exit()
+
+userinfo = requests.get("https://canary.discordapp.com/api/v9/users/@me", headers=headers).json()
+username = userinfo["username"]
+discriminator = userinfo["discriminator"]
+userid = userinfo["id"]
+
+def onliner(token, status):
+    ws = websocket.WebSocket()
+    ws.connect("wss://gateway.discord.gg/?v=9&encoding=json")
+    start = json.loads(ws.recv())
+    heartbeat = start["d"]["heartbeat_interval"]
+    auth = {
+        "op": 2,
+        "d": {
+            "token": token,
+            "properties": {
+                "$os": "Windows 10",
+                "$browser": "Google Chrome",
+                "$device": "Windows",
+            },
+            "presence": {"status": status, "afk": False},
+        },
+        "s": None,
+        "t": None,
+    }
+    ws.send(json.dumps(auth))
+    cstatus = {
+        "op": 3,
+        "d": {
+            "since": 0,
+            "activities": [
+                {
+                    "type": 4,
+                    "state": custom_status,
+                    "name": "broken",
+                    "id": "1157780161220055140",
+                }
+            ],
+            "status": status,
+            "afk": False,
+        },
+    }
+    ws.send(json.dumps(cstatus))
+    online = {"op": 1, "d": "None"}
+    time.sleep(heartbeat / 1000)
+    ws.send(json.dumps(online))
+
+def run_onliner():
+    os.system("clear")
+    print(f"Bem vindo {username}#{discriminator} ({userid}).")
+    while True:
+        onliner(usertoken, status)
+        time.sleep(30)
+
+keep_alive()
+run_onliner()
